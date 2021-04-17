ea1d4fec606224fbc5b9dd440e144f19922e1680
nbeshouri <nbeshouri@gmail.com>
2021-04-17 16:25:43 -0700

    UT at least runs with dynamic halting

--- universal_transformer/universal_transformer.py
+++ universal_transformer/universal_transformer.py
@@ -33,9 +33,12 @@ class UniversalTransformer(nn.Transformer):
 
 
 class UniversalTransformerEncoder(nn.Module):
-    def __init__(self, d_model, nhead, dropout=0.1, max_steps=2):
+    def __init__(self, d_model, nhead, dropout=0.1, max_steps=2, halting_threshold=1):
         super().__init__()
         self.max_steps = max_steps
+        self.halting_threshold = halting_threshold
+        if self.halting_threshold is not None:
+            self.halting_prob_predictor = nn.Sequential(nn.Linear(d_model, 1), nn.Sigmoid())
         self.positional_embedding = PositionalEncoding(d_model=d_model, dropout=0)
         self.temporal_embedding = TemporalEncoding(
             d_model=d_model, dropout=0, max_len=max_steps
@@ -62,42 +65,64 @@ class UniversalTransformerEncoder(nn.Module):
 
         """
         # TODO: Position embedding, Timing embedding, dynamic halting.
-        for step in range(self.max_steps):
+
+        def step_func(src, step):
             src = self.positional_embedding(src)
             src = self.temporal_embedding(src, step)
             scr2 = self.self_attn(
                 src, src, src, attn_mask=mask, key_padding_mask=src_key_padding_mask
             )[0]
-            scr = src + self.dropout1(scr2)
+            src = src + self.dropout1(scr2)
 
-            scr = self.norm1(scr)
+            scr = self.norm1(src)
             scr2 = self.transition(scr)
             src = src + self.dropout2(scr2)
 
             src = self.norm2(src)
+            return src
+
+        if self.halting_threshold is None:
+            src = self._run_fixed_loop(src, step_func)
+        else:
+            src = self._run_dynamic_halting_loop(src, step_func)
 
         return src
 
+    def _run_fixed_loop(self, state, step_func):
+        for step in range(self.max_steps):
+            state = step_func(state, step)
+        return state
 
-class UniversalTransformerDecoder(nn.Module):
-    def __init__(self, d_model, nhead, dropout=0.1, max_steps=2):
-        super().__init__()
-        self.max_steps = max_steps
-        self.positional_embedding = PositionalEncoding(
-            d_model=d_model, dropout=0, max_len=max_steps
-        )
-        self.temporal_embedding = TemporalEncoding(
-            d_model=d_model, dropout=0, max_len=max_steps
-        )
-        self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)
+    def _run_dynamic_halting_loop(self, state, step_func):
+        device = state.device
+        halting_probability = torch.zeros(state.size(0), state.size(1)).to(device)
+        n_updates = torch.zeros(state.size(0), state.size(1)).to(device)
+        remainders = torch.zeros(state.size(0), state.size(1)).to(device)
+        previous_state = torch.zeros_like(state).to(device)
+
+        step = 0
+        while ((halting_probability < self.halting_threshold) & (n_updates < self.max_steps)).any():
+            p = self.halting_prob_predictor(state).squeeze(-1)
+            still_running = (halting_probability < 1).float()
+            new_halted = (halting_probability + p * still_running > self.halting_threshold).float() * still_running
+            still_running = (halting_probability + p * still_running <= self.halting_threshold).float() * still_running
+            halting_probability += p * still_running
+            remainders += new_halted * (1 - halting_probability)
+            halting_probability += new_halted * remainders
+            n_updates += still_running + new_halted
+            update_weights = ((p * still_running) + (new_halted * remainders)).unsqueeze(-1)
+
+            state = step_func(state, step)
+            previous_state = ((state * update_weights) + (previous_state * (1 - update_weights)))
+            step += 1
+
+        return previous_state
+
+
+class UniversalTransformerDecoder(UniversalTransformerEncoder):
+    def __init__(self, d_model, nhead, dropout=0.1, max_steps=2, halting_threshold=1):
+        super().__init__(d_model=d_model, nhead=nhead, dropout=dropout, max_steps=max_steps, halting_threshold=halting_threshold)
         self.multihead_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)
-        self.dropout1 = nn.Dropout(dropout)
-        self.norm1 = nn.LayerNorm(d_model)
-        self.transition = nn.Sequential(
-            nn.Linear(d_model, d_model), nn.ReLU(), nn.Linear(d_model, d_model)
-        )
-        self.dropout2 = nn.Dropout(dropout)
-        self.norm2 = nn.LayerNorm(d_model)
         self.dropout3 = nn.Dropout(dropout)
         self.norm3 = nn.LayerNorm(d_model)
 
@@ -124,7 +149,7 @@ class UniversalTransformerDecoder(nn.Module):
             see the docs in Transformer class.
         """
         # TODO: Position embedding, Timing embedding, dynamic halting.
-        for step in range(self.max_steps):
+        def step_func(tgt, step):
             tgt = self.positional_embedding(tgt)
             tgt = self.temporal_embedding(tgt, step)
             tgt2 = self.self_attn(
@@ -147,6 +172,13 @@ class UniversalTransformerDecoder(nn.Module):
             tgt = tgt + self.dropout3(tgt2)
 
             tgt = self.norm3(tgt)
+            return tgt
+
+        if self.halting_threshold is None:
+            tgt = self._run_fixed_loop(tgt, step_func)
+        else:
+            tgt = self._run_dynamic_halting_loop(tgt, step_func)
+
         return tgt
 
 
