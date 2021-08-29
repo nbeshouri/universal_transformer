aab65335bd85af8924eb8c1c6190bea5a483ec93
nbeshouri <nbeshouri@gmail.com>
2021-08-29 14:43:00 -0700

    Fix tests

--- tests/test_train.py
+++ tests/test_train.py
@@ -8,7 +8,7 @@ from universal_transformer.wandb_utils import ConfigWrapper
 @pytest.mark.parametrize("config_file", ["universal_test.yaml", "wmt_test.yaml"])
 def test_train(config_file):
     os.environ["WANDB_MODE"] = "dryrun"
-    os.environ["WANDB_CONFIG_PATHS"] = f"../configs/{config_file}"
+    os.environ["WANDB_CONFIG_PATHS"] = f"configs/{config_file}"
     run = wandb.init(project="testing", reinit=True, mode="disabled")
     print(run)
     config = ConfigWrapper(wandb.config)
